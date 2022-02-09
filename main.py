49be252015151a8ae1674e6bfa886f70828c6207
tld98 <tld98@drexel.edu>
2022-02-08 19:24:38 -0500

    Obtain sentiments of messages in csv and append them to a list.

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-# This is a sample Python script.
+import csv
+import sys
+from nltk.sentiment import SentimentIntensityAnalyzer
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+maxInt = sys.maxsize
 
+while True:
+    # decrease the maxInt value by factor 10
+    # as long as the OverflowError occurs.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt/10)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #HELLO!
-    print_hi('Welcome')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sia = SentimentIntensityAnalyzer()
+    list_sentiments = []
+
+    with open('GitterCom.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            #print(row['message'])
+            list_sentiments.append(sia.polarity_scores(row['message']))
+
+    #for sentiment in list_sentiments:
+    #    print(sentiment)
+
+
+
+
+
